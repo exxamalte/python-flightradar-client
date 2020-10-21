34c70a363acda4b356bfa2e41f999075a7fe099a
Malte Franken <exxamalte@users.noreply.github.com>
2020-10-21 20:24:48 +1100

    exclude tests from final package

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/exxamalte/python-flightradar-client",
-    packages=find_packages(),
+    packages=find_packages(exclude=("tests*",)),
     classifiers=[
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
