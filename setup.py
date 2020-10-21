182bb015ea4e99d4b0cd3603ac02457d7f7120bf
Malte Franken <exxamalte@users.noreply.github.com>
2020-10-21 21:51:14 +1100

    Drop Python 3.5 support (#10)

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ setup(
     url="https://github.com/exxamalte/python-flightradar-client",
     packages=find_packages(exclude=("tests*",)),
     classifiers=[
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: Apache Software License",
