b283754071e6310573887731d3e4a400582634cd
Malte Franken <exxamalte@users.noreply.github.com>
2023-01-29 00:46:57 +1100

    migrated unit test

--- tests/test_statistics.py
+++ tests/test_statistics.py
@@ -1,44 +1,46 @@
 """Test for the statistics collector."""
-import asynctest
+import pytest as pytest
 
 from flightradar_client.statistics import Statistics, StatisticsData
 
 
-class TestStatistics(asynctest.TestCase):
-    """Test for the statistics collector."""
+@pytest.mark.asyncio
+async def test_basic_statistics(self):
+    """Test some basic statistics behaviour."""
+    statistics = Statistics()
+    assert repr(statistics) == "<Statistics[FixedSizeDict()]>"
 
-    async def test_basic_statistics(self):
-        """Test some basic statistics behaviour."""
-        statistics = Statistics()
-        assert repr(statistics) == "<Statistics[FixedSizeDict()]>"
+    value = statistics.get("non-existing")
+    self.assertIsNone(value)
 
-        value = statistics.get("non-existing")
-        self.assertIsNone(value)
 
-    async def test_basic_statistics_data(self):
-        """Test some basic statistics data behaviour."""
-        statistics_data = StatisticsData(False)
-        assert repr(statistics_data) == "<StatisticsData(0.0%)>"
-        statistics_data._total = 0
-        assert repr(statistics_data) == "<StatisticsData(0.0%)>"
-        # 1. successful update.
-        statistics_data.retrieval_successful()
-        assert repr(statistics_data) == "<StatisticsData(100.0%)>"
-        # 2. unsuccessful update.
-        statistics_data.retrieval_unsuccessful()
-        assert repr(statistics_data) == "<StatisticsData(50.0%)>"
+@pytest.mark.asyncio
+async def test_basic_statistics_data(self):
+    """Test some basic statistics data behaviour."""
+    statistics_data = StatisticsData(False)
+    assert repr(statistics_data) == "<StatisticsData(0.0%)>"
+    statistics_data._total = 0
+    assert repr(statistics_data) == "<StatisticsData(0.0%)>"
+    # 1. successful update.
+    statistics_data.retrieval_successful()
+    assert repr(statistics_data) == "<StatisticsData(100.0%)>"
+    # 2. unsuccessful update.
+    statistics_data.retrieval_unsuccessful()
+    assert repr(statistics_data) == "<StatisticsData(50.0%)>"
 
-    async def test_update(self):
-        """Test an update."""
-        statistics = Statistics()
-        KEYS = ["a", "b", "c"]
-        # 1. successful update.
-        await statistics.retrieval_successful(KEYS)
-        assert statistics.get("a").success_ratio() == 1.0
-        # 2. successful update.
-        await statistics.retrieval_successful(KEYS)
-        assert statistics.get("a").success_ratio() == 1.0
-        # 3. unsuccessful update.
-        await statistics.retrieval_unsuccessful()
-        self.assertAlmostEqual(statistics.get("a").success_ratio(), 0.666, 2)
-        assert repr(statistics.get("a")) == "<StatisticsData(66.7%)>"
+
+@pytest.mark.asyncio
+async def test_update(self):
+    """Test an update."""
+    statistics = Statistics()
+    KEYS = ["a", "b", "c"]
+    # 1. successful update.
+    await statistics.retrieval_successful(KEYS)
+    assert statistics.get("a").success_ratio() == 1.0
+    # 2. successful update.
+    await statistics.retrieval_successful(KEYS)
+    assert statistics.get("a").success_ratio() == 1.0
+    # 3. unsuccessful update.
+    await statistics.retrieval_unsuccessful()
+    self.assertAlmostEqual(statistics.get("a").success_ratio(), 0.666, 2)
+    assert repr(statistics.get("a")) == "<StatisticsData(66.7%)>"
